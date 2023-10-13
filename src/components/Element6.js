40761adef217c6d6614d21f63a9f884c8cd07dea
raoufard <a_derardja@estin.dz>
2023-10-13 17:37:09 +0100

    element6 completed

--- src/components/Element6.js
+++ src/components/Element6.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "../images/image 9.png"
+import {ReactComponent as Ele6Img} from "../images/image 9.svg"
 import {Showicons} from "./Iconearray";
 
 /* this code is for deleting an icon */
@@ -12,7 +12,7 @@ const sixIconcode = Showicons.slice(0, 6);
 export default function Element6(){
     return(
         <div className="element6-structure">
-            <img src={image} className="img-element6"/>
+            <Ele6Img className="img-element6"/>
             <div className="right-part-structure-element6">
                 <p className="description-element6">
                 Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.
